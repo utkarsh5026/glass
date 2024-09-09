d33c9c86c86daafdbc459d29b901850aa820a918
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-09-09 14:53:12 +0530

    changed style

--- client/src/components/courses/CourseOverview.tsx
+++ client/src/components/courses/CourseOverview.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Tabs } from "antd";
+import { Row, Col, Tabs, Button } from "antd";
 import {
   AppstoreOutlined,
   FileOutlined,
@@ -15,40 +15,66 @@ const CourseOverview: React.FC = () => {
   const onTabChange = (key: string) => {
     console.log(key);
   };
+
   return (
-    <Row
+    <div
       style={{
-        border: "1px solid white",
-        padding: "10px",
-        borderRadius: "10px",
-        width: "90vw",
         height: "90vh",
+        overflow: "auto",
+        position: "relative",
       }}
     >
-      <Col span={24}>
-        <Tabs onChange={onTabChange}>
-          <Tabs.TabPane tab="Announcements" key="1" icon={<AppstoreOutlined />}>
-            <Announcement />
-          </Tabs.TabPane>
-          <Tabs.TabPane
-            tab="Files"
-            key="2"
-            icon={<FileOutlined />}
-          ></Tabs.TabPane>
-          <Tabs.TabPane tab="People" key="3" icon={<UsergroupAddOutlined />}>
-            <CoursePeople />
-          </Tabs.TabPane>
-          <Tabs.TabPane
-            tab="Chat"
-            key="4"
-            icon={<MessageOutlined />}
-          ></Tabs.TabPane>
-          <Tabs.TabPane tab="Activity" key="5" icon={<CalendarOutlined />}>
-            <Activity />
-          </Tabs.TabPane>
-        </Tabs>
-      </Col>
-    </Row>
+      <Row
+        style={{
+          padding: "10px",
+          overflow: "auto",
+        }}
+      >
+        <Col span={24}>
+          <Tabs
+            animated={{
+              inkBar: true,
+              tabPane: true,
+            }}
+            onChange={onTabChange}
+            style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 1,
+              backgroundColor: "white",
+              paddingTop: "10px",
+            }}
+            tabBarExtraContent={{
+              right: <Button>Add</Button>,
+            }}
+          >
+            <Tabs.TabPane
+              tab="Announcements"
+              key="1"
+              icon={<AppstoreOutlined />}
+            >
+              <Announcement />
+            </Tabs.TabPane>
+            <Tabs.TabPane
+              tab="Files"
+              key="2"
+              icon={<FileOutlined />}
+            ></Tabs.TabPane>
+            <Tabs.TabPane tab="People" key="3" icon={<UsergroupAddOutlined />}>
+              <CoursePeople />
+            </Tabs.TabPane>
+            <Tabs.TabPane
+              tab="Chat"
+              key="4"
+              icon={<MessageOutlined />}
+            ></Tabs.TabPane>
+            <Tabs.TabPane tab="Activity" key="5" icon={<CalendarOutlined />}>
+              <Activity />
+            </Tabs.TabPane>
+          </Tabs>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
